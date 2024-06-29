9f164e6707d79ff354429ded1b54ee22f735f438
ENDE-ABAY <abayfeleke13@gmail.com>
2024-06-29 09:45:31 +0300

    layout-change

--- lib/app/ui/home_screen.dart
+++ lib/app/ui/home_screen.dart
@@ -14,7 +14,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _locationController = TextEditingController();
   List<Character> allCharacters = [];
   List<Character> displayedCharacters = [];
 
@@ -25,21 +24,18 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _searchCharacters(BuildContext context) {
     String nameFilter = _nameController.text.toLowerCase();
-    String locationFilter = _locationController.text.toLowerCase();
 
-    if (nameFilter.isEmpty && locationFilter.isEmpty) {
+    if (nameFilter.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            content: Text("Please enter a name or location to search")),
+        const SnackBar(content: Text("Please enter a name ")),
       );
       return;
     }
 
     List<Character> filteredCharacters = allCharacters.where((character) {
       bool matchesName = character.name.toLowerCase().contains(nameFilter);
-      bool matchesLocation =
-          character.location.toLowerCase().contains(locationFilter);
-      return matchesName && matchesLocation;
+
+      return matchesName;
     }).toList();
 
     Navigator.push(
@@ -70,154 +66,143 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              // Search Fields
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  children: [
-                    // Expanded(
-                    //   child: TextField(
-                    //     controller: _nameController,
-                    //     decoration: const InputDecoration(
-                    //       labelText: "Enter name",
-                    //       labelStyle: TextStyle(fontSize: 12),
-                    //       border: OutlineInputBorder(
-                    //         borderRadius: BorderRadius.all(Radius.circular(18.0)), // Rounded corners
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
-                    // const SizedBox(width: 8),
-                    Expanded(
-                      child: TextField(
-                        controller: _locationController,
-                        decoration: const InputDecoration(
-                          labelText: "Enter location",
-                          labelStyle: TextStyle(fontSize: 12),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                                Radius.circular(18.0)), // Rounded corners
-                          ),
+        child: Column(
+          children: [
+            // Search Fields Container
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width *
+                        0.6, // Decreased width
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        labelText: "Enter name",
+                        labelStyle: TextStyle(fontSize: 12),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(18.0)), // Rounded corners
                         ),
                       ),
                     ),
-                    const SizedBox(width: 8),
-                    ElevatedButton(
-                      onPressed: () => _searchCharacters(context),
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              18.0), // Adjust the value as needed
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 24.0, vertical: 12.0),
-                      ),
-                      child: const Text(
-                        "Search",
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // GraphQL Query and Results
-              Expanded(
-                child: Query(
-                  options: QueryOptions(
-                    fetchPolicy: FetchPolicy.cacheAndNetwork,
-                    document: gql(getAllCharacters()),
-                    variables: const {"page": 1},
                   ),
-                  builder: (result, {fetchMore, refetch}) {
-                    // We have data
-                    if (result.data != null) {
-                      int? nextPage = 1;
-                      List<Character> fetchedCharacters =
-                          (result.data!["characters"]["results"] as List)
-                              .map((e) => Character.fromMap(e))
-                              .toList();
-
-                      nextPage = result.data!["characters"]["info"]["next"];
-
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        _updateCharacters(fetchedCharacters);
-                      });
-
-                      return RefreshIndicator(
-                        onRefresh: () async {
-                          await refetch!();
-                          nextPage = 1;
-                        },
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              Center(
-                                child: Wrap(
-                                  spacing: 8,
-                                  runSpacing: 8,
-                                  children: displayedCharacters
-                                      .map((e) => CharacterWidget(character: e))
-                                      .toList(),
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 16,
-                              ),
-                              if (nextPage != null)
-                                ElevatedButton(
-                                  onPressed: () async {
-                                    FetchMoreOptions opts = FetchMoreOptions(
-                                      variables: {'page': nextPage},
-                                      updateQuery: (previousResultData,
-                                          fetchMoreResultData) {
-                                        final List<dynamic> repos = [
-                                          ...previousResultData!["characters"]
-                                              ["results"] as List<dynamic>,
-                                          ...fetchMoreResultData!["characters"]
-                                              ["results"] as List<dynamic>
-                                        ];
-                                        fetchMoreResultData["characters"]
-                                            ["results"] = repos;
-                                        return fetchMoreResultData;
-                                      },
-                                    );
-                                    await fetchMore!(opts);
-                                  },
-                                  child: result.isLoading
-                                      ? const CircularProgressIndicator
-                                          .adaptive()
-                                      : const Text("Load More"),
-                                ),
-                            ],
-                          ),
-                        ),
-                      );
-                    }
-                    // We got data but it is null
-                    else if (result.data == null) {
-                      return const Text("Data Not Found!");
-                    }
-                    // We don't have data yet -> LOADING STATE
-                    else if (result.isLoading) {
-                      return const Center(
-                        child: Text("Loading..."),
-                      );
-                    }
-                    // error state
-                    else {
-                      return const Center(
-                        child: Center(child: Text("Something went wrong")),
-                      );
-                    }
-                  },
-                ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () => _searchCharacters(context),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            18.0), // Adjust the value as needed
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24.0, vertical: 12.0),
+                    ),
+                    child: const Text(
+                      "Search",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 12),
+
+            // GraphQL Query and Results
+            Expanded(
+              child: Query(
+                options: QueryOptions(
+                  fetchPolicy: FetchPolicy.cacheAndNetwork,
+                  document: gql(getAllCharacters()),
+                  variables: const {"page": 1},
+                ),
+                builder: (result, {fetchMore, refetch}) {
+                  // We have data
+                  if (result.data != null) {
+                    int? nextPage = 1;
+                    List<Character> fetchedCharacters =
+                        (result.data!["characters"]["results"] as List)
+                            .map((e) => Character.fromMap(e))
+                            .toList();
+
+                    nextPage = result.data!["characters"]["info"]["next"];
+
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      _updateCharacters(fetchedCharacters);
+                    });
+
+                    return RefreshIndicator(
+                      onRefresh: () async {
+                        await refetch!();
+                        nextPage = 1;
+                      },
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Wrap(
+                                spacing: 8,
+                                runSpacing: 8,
+                                children: displayedCharacters
+                                    .map((e) => CharacterWidget(character: e))
+                                    .toList(),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            if (nextPage != null)
+                              ElevatedButton(
+                                onPressed: () async {
+                                  FetchMoreOptions opts = FetchMoreOptions(
+                                    variables: {'page': nextPage},
+                                    updateQuery: (previousResultData,
+                                        fetchMoreResultData) {
+                                      final List<dynamic> repos = [
+                                        ...previousResultData!["characters"]
+                                            ["results"] as List<dynamic>,
+                                        ...fetchMoreResultData!["characters"]
+                                            ["results"] as List<dynamic>
+                                      ];
+                                      fetchMoreResultData["characters"]
+                                          ["results"] = repos;
+                                      return fetchMoreResultData;
+                                    },
+                                  );
+                                  await fetchMore!(opts);
+                                },
+                                child: result.isLoading
+                                    ? const CircularProgressIndicator.adaptive()
+                                    : const Text("Load More"),
+                              ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }
+                  // We got data but it is null
+                  else if (result.data == null) {
+                    return const Text("Data Not Found!");
+                  }
+                  // We don't have data yet -> LOADING STATE
+                  else if (result.isLoading) {
+                    return const Center(
+                      child: Text("Loading..."),
+                    );
+                  }
+                  // error state
+                  else {
+                    return const Center(
+                      child: Center(child: Text("Something went wrong")),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
